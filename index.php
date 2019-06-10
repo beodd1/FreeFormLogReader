66500e97ab28c2cae179918241c22280ff4d295e
beodd <beodd@DevStation>
2019-06-09 23:01:09 -0500

    ...

--- index.php
+++ index.php
@@ -6,49 +6,7 @@
     <script src="js/jquery.iframe-transport.js"></script>
     <script src="js/jquery.fileupload.js"></script>
     <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
-    <style>
- 
-     
- 
-    #drop_file_zone {
-        background-color: #EEE; 
-        border: #999 5px dashed;
-        width: 100%; 
-        height: 100%;
-        padding: 8px;
-        font-size: 18px;
-    }
-    
-    .fileinput-button {
-    position: relative;
-    overflow: hidden;
-    display: inline-block;
-    }
-    
-  .fileinput-button input {
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 0;
-    opacity: 0;
-    -ms-filter: 'alpha(opacity=0)';
-    font-size: 200px !important;
-    direction: ltr;
-    cursor: pointer;
-    }
 
-  /* Fixes for IE < 8 */
-  @media screen\9 {
-    .fileinput-button input {
-      filter: alpha(opacity=0);
-      font-size: 100%;
-      height: 100%;
-    }
-}
-
-    
-    
-    </style>
     <script>
 
 
@@ -67,6 +25,7 @@
                 $.each(data.result.files, function (index, file) {
                     $('<p/>').text(file.name).appendTo('#files');
                     document.getElementById('files_').value = file.name;
+                    //document.getElementById('search_').value = '';
                     document.getElementById('FileNamePostBack').submit();
                     
                     //alert(file.name);
@@ -102,13 +61,180 @@
     
     
     </script>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+<style>
+
+
+.bottomnavbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+
+.bottomnavbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.bottomnavbar a:hover {
+  background: #ddd;
+  color: black;
+}        
+        
+        
+/* The navigation bar */
+.topnavbar {
+  overflow: hidden;
+  background-color: #333;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+}
+        
+     
+        
+/* Links inside the navbar */
+.topnavbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+/* Change background on mouse-over */
+
+
+/* Main content */
+.main {
+  margin-top: 30px; /* Add a top margin to avoid content overlay */
+}
+ 
+    #drop_file_zone {
+        background-color: #EEE; 
+        border: #999 5px dashed;
+        width: 100%; 
+        height: 100%;
+        padding: 8px;
+        font-size: 20px;
+    }
+    
+    .fileinput-button {
+    position: relative;
+    overflow: hidden;
+    display: inline-block;
+    }
+    
+  .fileinput-button input {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 0;
+    opacity: 0;
+    -ms-filter: 'alpha(opacity=0)';
+    font-size: 200px !important;
+    direction: ltr;
+    cursor: pointer;
+    }
+
+  /* Fixes for IE < 8 */
+  @media screen\9 {
+    .fileinput-button input {
+      filter: alpha(opacity=0);
+      font-size: 100%;
+      height: 100%;
+    }
+}
+
+
+
+.search {
+  width: 100%;
+  position: relative;
+  display: flex;
+}
+
+.searchTerm {
+  width: 100%;
+  border: 3px solid #555555;
+  /* border-right: none; */
+  padding: 4px;
+  height: 30px;
+  border-radius: 10px 0 0 10px;
+  outline: none;
+  color: #515151;
+}
+
+.searchTerm:focus{
+  color: #515151;
+}
+
+.searchButton {
+  width: 50px;
+  height: 28px;
+  border: 1px solid #515151;
+  background: #515151;
+  text-align: center;
+  color: #515151;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+  font-size: 20px;
+}
+
+/*Resize the wrap to see the search bar change!*/
+.wrap{
+  width: 40%;
+  position: absolute;
+  top: 28px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+
+
+    
+    
+    </style>
+    
+    
+    
 </head>
 <body>
+
+<div class="topnavbar">
+  <a href="#home">Free Form Log Reader</a>
+
+  
+  
+</div>
+
+
     
-<form id="FileNamePostBack" action="index.php" method="post" >
-    <input hidden="true" type="text" name="files_"  id="files_" class="files"/>
-</form>
-    
+<div class="bottomnavbar">
+    <a href="#home"><input type="checkbox"/> Critical</a>
+  <a href="#news"><input type="checkbox"/>Error</a>
+  <a href="#contact"><input type="checkbox"/>Warning</a>
+  <a href="#contact"><input type="checkbox"/>Everything</a>
+</div>    
     
     <div id="drop_file_zone" class="container">
         
@@ -116,8 +242,7 @@
             <div class="container">
                 <div class="row">
                     <div  class="col-sm-9">
-                        <h2>Free Form Log Reader</h2>
-                        <p>A Log Reader For Everyone</p>
+
                     </div>
                     <div   class="col-sm-3">
                         <br>
@@ -127,25 +252,79 @@
                             <input id="fileupload" type="file" name="files[]" multiple>
                         </span>
                     </div>
-                </div>
+                </div><br>
                 <div style="height: 5px;" id="progress" class="progress">
                     <div style="height: 5px;" class="progress-bar progress-bar-success"></div>
                 </div>
             </div>
-        <div style="overflow:scroll; height:70%; width: 98%" >
+            
+                    
+<form id="FileNamePostBack" action="index.php" method="post" >
+    <input hidden="true" type="text" name="files_"  id="files_" class="files"
+        <?php 
+                if(!empty($_POST['files_']))
+                {
+                    echo " value=" . $_POST['files_'] . " ";
+                }  
+        ?> />
+
+        <div class="wrap">
+           <div class="search">
+               
+              <input type="text" name="search_" class="searchTerm" placeholder="Search For Something"
+                <?php 
+                if(!empty($_POST['search_']))
+                {
+                    echo " value=" . $_POST['search_'] . " ";
+                }  
+                ?>/>
+                >
+              <button type="submit" class="searchButton">
+                  <img src="img/searchico.png" height="20" width="20" alt=""/>
+             </button>
+                     
+           </div>
+            
+        </div>
+
+</form>
+
+
+
+            
+        <div style="overflow:scroll; height:70%; width: 98%;" >
             <?php 
                 //var_dump($_POST);
                 if(!empty($_POST['files_']))
                 {
+                    
                     $file = new SplFileObject("./server/php/files/". $_POST['files_'] );
                     
                     echo "<h5><ul>";
+                    
+                    
+                    
                     while (!$file->eof()) 
                     {
                         $file->fgets();
-                        echo "<li>";
-                        echo $file;
-                        echo "</li>"; 
+                        
+                        if(!empty($_POST['search_']))
+                        {
+                            $search_ = $_POST['search_'];
+                            if (strpos($file, $search_)) // Filter for search string
+                            {
+                                echo "<li style='background-color: #f7ffc6;'>";
+                                echo $file;
+                                echo "</li>";
+                                continue;
+                            }  
+                        }
+                        //else 
+                        //{
+                            echo "<li style='list-style: none'>";
+                            echo $file;
+                            echo "</li>";
+                        //}
                         continue;
                     }
                     echo "</ul></h5>";
@@ -155,6 +334,10 @@
         </div>
             
     </div>
+
+    
+
+        
 </body>
 
 
